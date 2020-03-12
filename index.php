2396ca28554627a7cea0c14bc72f96f49f0e254a
beatrizf13 <azz.felix7@gmail.com>
2020-03-12 11:58:12 -0300

    routes

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
 
+require_once (__DIR__) . "/app/config/env.php";
 require_once "vendor/autoload.php";
 
 use Slim\App;
-use Controllers\SessionController;
+use Controllers;
+use Tuupola\Middleware\JwtAuthentication;
 
 $config = ['settings' => [
     'addContentLengthHeader' => false,
@@ -12,8 +14,18 @@ $config = ['settings' => [
 
 $app = new App($config);
 
+$app->post('/register', [Controllers\UserController::class, "store"]);
 
-$app->get('/login', [SessionController::class, "login"]);
+$app->post('/login', [Controllers\SessionController::class, "login"]);
 
+$app->post('/refresh-token', [Controllers\SessionController::class, "refreshToken"]);
+
+$app->get('/listar-produtos', function ($req, $res, $args) {
+    $products = [
+        "products" => ["book", "table", "computer", "mouse"]
+    ];
+
+    return $res->withJson($products);
+})->add(new JwtAuthentication(["secret" => getenv("JWT_SECRET")]));
 
 $app->run();
